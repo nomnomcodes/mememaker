8e0dd222e8affb381905774363fb8bfaf5da3840
nomnomcodes <nomnomcodes@gmail.com>
2024-11-15 00:18:53 +0000

    scr

--- script.js
+++ script.js
@@ -176,7 +176,7 @@ document.getElementById('dropShadow').addEventListener('change', function() {
   });
 
   const folders = {
-    general:64,
+    general:67,
     cartoon: 90,
     brands: 11,
     gang: 18,
